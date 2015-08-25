237ac4811234afc34e03d0ce91487412a420fb8e
danigrane <danigrane@gmail.com>
2015-08-24 21:35:57 -0300

    daleeeee

--- selenium/src/main/java/com/blackwolves/selenium/seeder/SeederRunnable.java
+++ selenium/src/main/java/com/blackwolves/selenium/seeder/SeederRunnable.java
@@ -60,28 +60,23 @@ public class SeederRunnable implements Runnable {
 //				WebDriver driver = new HtmlUnitDriver(capability);
 //				WebDriver driver = new HtmlUnitDriver(true);
 				WebDriver driver = new HtmlUnitDriver();
+//				WebDriver driver = new FirefoxDriver();
+//				WebDriver driver = new ChromeDriver();
+//				WebDriver driver = new InternetExplorerDriver();
 				
 				yahooLogin(yahooUrl, seed, driver);
+				
+				validateYahooVersion(driver, seed);
 		        
-		        processBulk(driver);
+		        processBulk(driver, seed);
 		        
-		        processInbox(driver);
+		        processInbox(driver, seed);
 		        
 		        logger.info("Finished!!");
 
-//		        logger.info(driver.getPageSource());		        
-//		        Select all spams and move them to inbox
-//		        if(driver.findElements(By.id("select_all")).size() > 0){
-//		        	
-//		        	logger.info("Clicking Select all checkbox");
-//			        driver.findElement(By.id("select_all")).click();
-//			        
-//			        if(driver.findElements(By.id("top_ham")).size() > 0){
-//			        	
-//				        logger.info("Clicking Not spam button");
-//				        driver.findElement(By.id("top_ham")).click();
-//			        }
-//		        }
+//		        logger.info(driver.getPageSource());
+		        
+//		        clickNotSpamForAllMessages(driver);
 		        
 			}catch(NoSuchElementException nse){
 				logger.error(nse.getMessage(), nse);
@@ -111,95 +106,33 @@ public class SeederRunnable implements Runnable {
 		logger.info("Clicking login button");
 		driver.findElement(By.id("login-signin")).click();
 	}
-
+	
 	/**
+	 * 
 	 * @param driver
-	 * @throws InterruptedException
+	 * @param seed 
 	 */
-	private void processInbox(WebDriver driver) throws InterruptedException {
-		if(driver.findElements(By.id("inbox")).size() > 0){
-			
-			logger.info("Getting the Inbox Url");
-			driver.get(driver.findElement(By.id("inbox")).findElement(By.tagName("a")).getAttribute("href"));
-			
-			if(driver.findElements(By.className("mlink")).size() > 0){
-		    	
-				logger.info("mlink found");
-		    	List<WebElement> inboxMsgs = driver.findElements(By.className("mlink"));
-		    	
-		    	int percentage = (int) (inboxMsgs.size() * PERCENTAGE);
-		    	for(int j = 0 ; j < percentage; j++){
-		    		if(driver.findElements(By.className("mlink")).size() != 0){
-		    			inboxMsgs = driver.findElements(By.className("mlink"));
-			    		Random randomNo = new Random();
-			    		int randomPosition = randomNo.nextInt(inboxMsgs.size()>=50?50:inboxMsgs.size());
-			        	String href = inboxMsgs.get(randomPosition).getAttribute("href");
-						driver.get(href);
-						clickShowImages(driver, "show-text");
-//						scrollDownAndUp(driver);
-						clickRandomLink(driver);
-						
-//						Returns to inbox
-						driver.get(driver.findElement(By.id("inbox")).findElement(By.tagName("a")).getAttribute("href"));
-		    		}
-						
-		    	}
-		    }
-		}
-	}
-
-	/**
-	 * @param driver
-	 * @throws InterruptedException
-	 */
-	private void clickRandomLink(WebDriver driver) throws InterruptedException {
-		WebElement div = driver.findElement(By.className("mailContent"));
-		if(div.findElements(By.tagName("a")).size()!=0){
-			List<WebElement> linksToGo = div.findElements(By.tagName("a"));
-			Random rand = new Random();
-			int randomLinkNo = rand.nextInt(linksToGo.size());
-			String aUrl = linksToGo.get(randomLinkNo).getAttribute("href");
-			if(aUrl!=null){
-				if(aUrl.contains("unsub") || aUrl.contains("yahoo")){
-					logger.info("Unsubscribe link!!");
-					logger.info(aUrl);
-				}else{
-					openInNewWindow(driver, linksToGo.get(randomLinkNo));
-				}
-			}
-		}
-	}
-
-	/**
-	 * @param driver
-	 */
-	private void scrollDownAndUp(WebDriver driver) {
-//		scroll down
-		JavascriptExecutor jse = (JavascriptExecutor)driver;
-//		jse.executeScript("window.scrollBy(0,250)", "");
-//		OR
-		jse.executeScript("scroll(0, 250);");
-//		scroll up
-//		jse.executeScript("window.scrollBy(0,-250)", "");
-//		OR
-		jse.executeScript("scroll(0, -250);");
-	}
-
-	private void clickShowImages(WebDriver driver, String className) {
-		if(validateInboxShowImagesButton(driver, className)){
-			driver.findElement(By.className(className)).findElement(By.tagName("a")).click();
+	private void validateYahooVersion(WebDriver driver, String[] seed) {
+		if(driver.findElements(By.className("uh-srch-btn")).size() > 0){
+			logger.info("**********   Old yahoo version   **********");
+		}else if(driver.findElements(By.id("UHSearchProperty")).size() > 0){
+			logger.info("**********   New yahoo 2 version   **********");
+			SuscriberRunnable.writeToFile(seed[0] + "/" + "new_yahoo_2_version.html", driver.getPageSource());
+		}else if(driver.findElements(By.id("mail-search-btn")).size() > 0){
+			logger.info("**********   New yahoo version   **********");
+			SuscriberRunnable.writeToFile(seed[0] + "/" + "new_yahoo_version.html", driver.getPageSource());
+		}else{
+			logger.info("**********   There is a new yahoo version in town  **********");
+			SuscriberRunnable.writeToFile(seed[0] + "/" + "new_version_in_town.html", driver.getPageSource());
 		}
 		
 	}
-
-	private boolean validateInboxShowImagesButton(WebDriver driver, String className) {
-		return driver.findElements(By.className(className)).size() != 0;
-	}
-
+	
 	/**
 	 * @param driver
+	 * @param seed 
 	 */
-	private void processBulk(WebDriver driver) {
+	private void processBulk(WebDriver driver, String[] seed) {
 		if(driver.findElements(By.id("bulk")).size() > 0){
 		
 		    logger.info("Getting the Bulk Url");
@@ -212,22 +145,142 @@ public class SeederRunnable implements Runnable {
 		    	
 		    	int percentage = (int) (spamMsgs.size() * PERCENTAGE);
 		    	for(int j = 0 ; j < percentage; j++){
-		    		if(driver.findElements(By.className("mlink")).size()!=0){
+		    		if(driver.findElements(By.className("mlink")).size() > 0){
 		    			spamMsgs = driver.findElements(By.className("mlink"));
-			    		Random randomNo = new Random();
-			    		int randomPosition = randomNo.nextInt(spamMsgs.size()>=50?50:spamMsgs.size());
+		    			
+		    			logger.info("Obtaining a random message position so it can be open");
+			    		int randomPosition = obtainRandomMsgsPosition(spamMsgs);
 		        		String href = spamMsgs.get(randomPosition).getAttribute("href");
-						logger.info(href);
+						
+		        		logger.info("Opening this spam message: " + href);
 						driver.get(href);
+						
 						clickShowImages(driver, "spamwarning");
+						
 						Select notSpam = new Select(driver.findElement(By.name("top_action_select")));
 						notSpam.selectByValue("msg.ham");
+						logger.info("Clicking the not spam option");
 						driver.findElement(By.name("self_action_msg_topaction")).click();
-		    		}
+		    		}else{
+				    	logger.info("**********   No mlink found or no messages available   **********");
+				    }
 		    	}
+		    }else{
+		    	logger.info("**********   No mlink found or no messages available   **********");
 		    }
-
+		}else{
+			logger.info("**********   No bulk Url found   **********");
+			SuscriberRunnable.writeToFile(seed[0] + "/" + "now_bulk_url.html", driver.getPageSource());
 		}
+	}
+
+	/**
+	 * @param driver
+	 * @param seed 
+	 * @throws InterruptedException
+	 */
+	private void processInbox(WebDriver driver, String[] seed) throws InterruptedException {
+		if(driver.findElements(By.id("inbox")).size() > 0){
+			
+			logger.info("Getting the Inbox Url");
+			driver.get(driver.findElement(By.id("inbox")).findElement(By.tagName("a")).getAttribute("href"));
+			
+			if(driver.findElements(By.className("mlink")).size() > 0){
+		    	
+				logger.info("mlink found");
+		    	List<WebElement> inboxMsgs = driver.findElements(By.className("mlink"));
+		    	
+		    	int percentage = (int) (inboxMsgs.size() * PERCENTAGE);
+		    	for(int j = 0 ; j < percentage; j++){
+		    		if(driver.findElements(By.className("mlink")).size() >  0){
+		    			inboxMsgs = driver.findElements(By.className("mlink"));
+		    			logger.info("Obtaining a random message position so it can be open");
+			    		int randomPosition = obtainRandomMsgsPosition(inboxMsgs);
+			        	String href = inboxMsgs.get(randomPosition).getAttribute("href");
+			        	logger.info("Opening this inbox message: " + href);
+						driver.get(href);
+						clickShowImages(driver, "show-text");
+						
+//						scrollDownAndUp(driver);
+						
+						clickRandomLink(driver);
+						
+						logger.info("Going back to inbox");
+						driver.get(driver.findElement(By.id("inbox")).findElement(By.tagName("a")).getAttribute("href"));
+		    		}else{
+				    	logger.info("**********   No mlink found or no messages available   **********");
+				    }
+		    	}
+		    }else{
+		    	logger.info("**********   No mlink found or no messages available   **********");
+		    }
+		}else{
+			logger.info("**********   No inbox Url found   **********");
+			SuscriberRunnable.writeToFile(seed[0] + "/" + "no_inbox_url.html", driver.getPageSource());
+		}
+	}
+	
+	/**
+	 * @param spamMsgs
+	 * @return
+	 */
+	private int obtainRandomMsgsPosition(List<WebElement> spamMsgs) {
+		Random randomNo = new Random();
+		int randomPosition = randomNo.nextInt(spamMsgs.size()>=50?50:spamMsgs.size());
+		return randomPosition;
+	}
+
+	/**
+	 * @param driver
+	 * @throws InterruptedException
+	 */
+	private void clickRandomLink(WebDriver driver) throws InterruptedException {
+		logger.info("Getting the content of the message");
+		WebElement div = driver.findElement(By.className("mailContent"));
+		
+		logger.info("Looking for links inside the message");
+		if(div.findElements(By.tagName("a")).size()!=0){
+			logger.info("Links found");
+			List<WebElement> linksToGo = div.findElements(By.tagName("a"));
+			Random rand = new Random();
+			int randomLinkNo = rand.nextInt(linksToGo.size());
+			String aUrl = linksToGo.get(randomLinkNo).getAttribute("href");
+			if(aUrl!=null){
+				if(aUrl.contains("unsub") || aUrl.contains("yahoo")){
+					logger.info("It is an Unsubscribe link!! - we are not clicking it");
+					logger.info(aUrl);
+				}else{
+					openInNewWindow(driver, linksToGo.get(randomLinkNo));
+				}
+			}
+		}else{
+			logger.info("**********   No links found or none available  **********");
+		}
+	}
+
+	/**
+	 * 
+	 * @param driver
+	 * @param className
+	 */
+	private void clickShowImages(WebDriver driver, String className) {
+		if(validateInboxShowImagesButton(driver, className)){
+			logger.info("Clicking the show images button");
+			driver.findElement(By.className(className)).findElement(By.tagName("a")).click();
+		}else{
+			logger.info("**********   No show images button found or there is none   **********");
+		}
+		
+	}
+
+	/**
+	 * 
+	 * @param driver
+	 * @param className
+	 * @return
+	 */
+	private boolean validateInboxShowImagesButton(WebDriver driver, String className) {
+		return driver.findElements(By.className(className)).size() > 0;
 	}
 
 	/**
@@ -262,6 +315,51 @@ public class SeederRunnable implements Runnable {
 		}
 	}
 	
+	private static int randInt(int min, int max) {
+
+	    // Usually this can be a field rather than a method variable
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+	
+	/**
+	 * @param driver
+	 */
+	private void scrollDownAndUp(WebDriver driver) {
+//		scroll down
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+//		jse.executeScript("window.scrollBy(0,250)", "");
+//		OR
+		jse.executeScript("scroll(0, 250);");
+//		scroll up
+//		jse.executeScript("window.scrollBy(0,-250)", "");
+//		OR
+		jse.executeScript("scroll(0, -250);");
+	}
+	
+	/**
+	 * @param driver
+	 */
+	private void clickNotSpamForAllMessages(WebDriver driver) {
+		//		        Select all spams and move them to inbox
+		if(driver.findElements(By.id("select_all")).size() > 0){
+			
+			logger.info("Clicking Select all checkbox");
+		    driver.findElement(By.id("select_all")).click();
+		    
+		    if(driver.findElements(By.id("top_ham")).size() > 0){
+		    	
+		        logger.info("Clicking Not spam button");
+		        driver.findElement(By.id("top_ham")).click();
+		    }
+		}
+	}
+	
 	/**
 	 * 
 	 * @param ip
@@ -293,16 +391,5 @@ public class SeederRunnable implements Runnable {
         
         return capability;
     }
-	public static int randInt(int min, int max) {
-
-	    // Usually this can be a field rather than a method variable
-	    Random rand = new Random();
-
-	    // nextInt is normally exclusive of the top value,
-	    // so add 1 to make it inclusive
-	    int randomNum = rand.nextInt((max - min) + 1) + min;
-
-	    return randomNum;
-	}
 
 }
