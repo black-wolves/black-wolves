ce08636835103abe1415b65c4eb4a0cd3a893148
gastondapice <gastondapice@gmail.com>
2015-09-07 15:27:27 -0300

    subscriptions modified

--- black-wolves-seeder/src/main/java/com/blackwolves/subscriber/SubscriberRunnable.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/subscriber/SubscriberRunnable.java
@@ -36,6 +36,10 @@ public class SubscriberRunnable {
 		
 	}
 
+	/**
+	 * 
+	 * @param mySeed
+	 */
 	public void runProcess(String mySeed) {
 		String[] seed = mySeed.split(",");
 		Seed dbSeed = seedService.getSeedFromDb(seed);
@@ -46,54 +50,28 @@ public class SubscriberRunnable {
 		caps.setCapability("resolution", "1280x800");
 		WebDriver driver = new FirefoxDriver(caps);
 		try {
-			suscribeToSkimm(dbSeed, driver);
-//			suscribeToMatterMark(seed, driver);
-//			suscribeFashionMagazine(seed, driver);
-//			suscribeToDigg(seed, driver);
-			// suscribeToTheWeek(seed, driver);
+			subscribeToSkimm(dbSeed, driver);
+			subscribeToMatterMark(dbSeed, driver);
+			subscribeFashionMagazine(dbSeed, driver);
+			subscribeToDigg(dbSeed, driver);
+			subscribeToTheWeek(dbSeed, driver);
 
 			driver.close();
 		}
 
-		catch (NoSuchElementException nse)
-		{
-			logger.error(nse.getMessage(), nse);
+		catch (NoSuchElementException e) {
+			logger.error(e.getMessage(), e);
 			driver.close();
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			driver.close();
 		}
-
 	}
-
-	// works :)
-	private void suscribeFashionMagazine(String[] seed, WebDriver driver) throws InterruptedException {
-		driver.get("http://www.fashionmagazine.com/");
-		driver.findElement(By.name("input_1")).clear();
-		;
-		driver.findElement(By.name("input_1")).sendKeys(seed[0]);
-		driver.findElement(By.name("input_1")).submit();
-		if (driver.getPageSource().contains("Thanks")) {
-			logger.info("Fashion Magazine Suscription succesful.");
-		}
-
-	}
-
-	// Works :)
-	private void suscribeToMatterMark(String[] seed, WebDriver driver) throws InterruptedException {
-
-		driver.get("http://mattermark.com/app/Newsletter");
-		driver.findElement(By.id("free_email")).sendKeys(seed[0]);
-		driver.findElement(By.id("free_email")).submit();
-		Thread.sleep(1000);
-
-	}
-
+	
 	// Works! :)
-	private void suscribeToSkimm(Seed dbSeed, WebDriver driver) throws InterruptedException {
+	private void subscribeToSkimm(Seed dbSeed, WebDriver driver) throws InterruptedException {
 		String url = "http://www.theskimm.com/";
-		if(isSubscribed(dbSeed, url)){
+		if (isSubscribed(dbSeed, url)) {
 			logger.info("This seed is already subscribe to: " + url);
 			return;
 		}
@@ -110,7 +88,104 @@ public class SubscriberRunnable {
 		} catch (ServiceException e) {
 			logger.error(e.getMessage(), e);
 		}
-		Thread.sleep(1000);
+		Thread.sleep(3000);
+	}
+	
+	// Works :)
+	private void subscribeToMatterMark(Seed dbSeed, WebDriver driver) throws InterruptedException {
+		String url = "http://mattermark.com/app/Newsletter";
+		if (isSubscribed(dbSeed, url)) {
+			logger.info("This seed is already subscribe to: " + url);
+			return;
+		}
+		logger.info("Subscribing to Mattermark");
+		driver.get(url);
+		driver.findElement(By.id("free_email")).sendKeys(dbSeed.getEmail());
+		driver.findElement(By.id("free_email")).submit();
+		Subscription subscription = new Subscription("Mattermark", url);
+		dbSeed.getSubscriptions().add(subscription);
+		try {
+			seedService.saveOrUpdate(dbSeed);
+			logger.info("Successfully subscribed to: " + url);
+		} catch (ServiceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		Thread.sleep(3000);
+
+	}
+
+	// Works :)
+	private void subscribeFashionMagazine(Seed dbSeed, WebDriver driver) throws InterruptedException {
+		String url = "http://www.fashionmagazine.com/";
+		if (isSubscribed(dbSeed, url)) {
+			logger.info("This seed is already subscribe to: " + url);
+			return;
+		}
+		logger.info("Subscribing to Fashion Magazine");
+		driver.get(url);
+		driver.findElement(By.name("input_1")).clear();
+		driver.findElement(By.name("input_1")).sendKeys(dbSeed.getEmail());
+		driver.findElement(By.name("input_1")).submit();
+		if (driver.getPageSource().contains("Thanks")) {
+			logger.info("Fashion Magazine Subscription succesful.");
+		}
+		Subscription subscription = new Subscription("Fashion Magazine", url);
+		dbSeed.getSubscriptions().add(subscription);
+		try {
+			seedService.saveOrUpdate(dbSeed);
+			logger.info("Successfully subscribed to: " + url);
+		} catch (ServiceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		Thread.sleep(3000);
+	}
+	
+	// Works! :)
+	private void subscribeToDigg(Seed dbSeed, WebDriver driver) throws InterruptedException {
+		String url = "http://digg.com/";
+		if (isSubscribed(dbSeed, url)) {
+			logger.info("This seed is already subscribe to: " + url);
+			return;
+		}
+		logger.info("Subscribing to Digg");
+		driver.get(url);
+		driver.findElement(By.name("email")).sendKeys(dbSeed.getEmail());
+		driver.findElement(By.id("daily-digg-email-submit-btn")).click();
+		if (driver.getPageSource().contains("Thanks for subscribing!")) {
+			logger.info("Digg Subscription succesful.");
+		}
+		Subscription subscription = new Subscription("Digg", url);
+		dbSeed.getSubscriptions().add(subscription);
+		try {
+			seedService.saveOrUpdate(dbSeed);
+			logger.info("Successfully subscribed to: " + url);
+		} catch (ServiceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		Thread.sleep(3000);
+	}
+	
+	// Works! :)
+	private void subscribeToTheWeek(Seed dbSeed, WebDriver driver) throws InterruptedException {
+		String url = "http://theweek.com/";
+		if (isSubscribed(dbSeed, url)) {
+			logger.info("This seed is already subscribe to: " + url);
+			return;
+		}
+		logger.info("Subscribing to The Week");
+		driver.get(url);
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(dbSeed.getEmail());
+		driver.findElement(By.name("email")).submit();
+		Subscription subscription = new Subscription("The Week", url);
+		dbSeed.getSubscriptions().add(subscription);
+		try {
+			seedService.saveOrUpdate(dbSeed);
+			logger.info("Successfully subscribed to: " + url);
+		} catch (ServiceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		Thread.sleep(3000);
 	}
 
 	/**
@@ -128,29 +203,11 @@ public class SubscriberRunnable {
 		return false;
 	}
 
-	private void suscribeToDigg(String[] seed, WebDriver driver) {
-
-		driver.get("http://digg.com/");
-
-		driver.findElement(By.name("email")).sendKeys(seed[0]);
-
-		driver.findElement(By.id("daily-digg-email-submit-btn")).click();
-		if (driver.getPageSource().contains("Thanks for subscribing!")) {
-			logger.info("Suscribed " + seed[0] + " to Digg!");
-		}
-
-	}
-
-	private void suscribeToTheWeek(String[] seed, WebDriver driver) throws InterruptedException {
-		driver.get("http://theweek.com/");
-		driver.findElement(By.name("email")).clear();
-		;
-		driver.findElement(By.name("email")).sendKeys(seed[0]);
-		driver.findElement(By.name("email")).submit();
-		Thread.sleep(1000);
-
-	}
-
+	/**
+	 * 
+	 * @param fileName
+	 * @param fileContent
+	 */
 	public static void writeToFile(String fileName, String fileContent) {
 		try {
 			logger.info("Writing page to: " + fileName);
