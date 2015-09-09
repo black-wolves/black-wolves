fa47ca5cab1ed0f9e8b1c7ae3e5d7252caf48a67
gastondapice <gastondapice@gmail.com>
2015-09-09 20:27:55 -0300

    open in new window modified

--- black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -20,7 +19,6 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.MoveTargetOutOfBoundsException;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -163,36 +161,39 @@ public abstract class YahooRunnable {
 	 * @throws InterruptedException
 	 */
 	protected void openInNewWindow(WebElement a) {
-
-		logger.info("Cicking this link: " + a.getAttribute("href"));
-		Actions newTab = new Actions(driver);
 		try{
+			logger.info("Cicking this link: " + a.getAttribute("href"));
+			Actions newTab = new Actions(driver);
 			newTab.keyDown(Keys.SHIFT).click(a).keyUp(Keys.SHIFT).build().perform();
-		} catch (MoveTargetOutOfBoundsException e){
-			logger.error(e.getMessage(), e);
-		}
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			Thread.sleep(5000);
 
-		// handle windows change
-		String base = driver.getWindowHandle();
-		Set<String> set = driver.getWindowHandles();
+			// handle windows change
+			String base = driver.getWindowHandle();
+			Set<String> set = driver.getWindowHandles();
 
-		set.remove(base);
-		assert set.size() == 1;
-		try{
+			set.remove(base);
+			assert set.size() == 1;
 			driver.switchTo().window((String) set.toArray()[0]);
-		} catch (ArrayIndexOutOfBoundsException e){
+
+			// close the window
+			driver.close();
+			driver.switchTo().window(base);
+
+			// handle windows change and switch back to the main window
+			Thread.sleep(2500);
+			for (String winHandle : driver.getWindowHandles()) {
+				driver.switchTo().window(winHandle);
+			}
+		} catch (InterruptedException e) {
 			logger.error(e.getMessage(), e);
-		}
-
-		// close the window
-		driver.close();
-		driver.switchTo().window(base);
-
-		// handle windows change and switch back to the main window
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		for (String winHandle : driver.getWindowHandles()) {
-			driver.switchTo().window(winHandle);
+		} catch (NoSuchElementException e) {
+			logger.error(e.getMessage(), e);
+		} catch (StaleElementReferenceException e) {
+			logger.error(e.getMessage(), e);
+		} catch (ElementNotVisibleException e) {
+			logger.error(e.getMessage(), e);
+		} catch (ElementNotFoundException e) {
+			logger.error(e.getMessage(), e);
 		}
 	}
 	
