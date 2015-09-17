a9e0a01c9628d6bc945c84ecf8036f518ebe1f6e
gastondapice <gastondapice@gmail.com>
2015-09-17 18:54:52 -0300

    a

--- black-wolves-seeder/src/main/java/com/blackwolves/thread/SeederThreadPool.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/thread/SeederThreadPool.java
@@ -26,10 +26,10 @@ public class SeederThreadPool {
 		logger.info("Starting SeederThreadPool");
 		ExecutorService executor = Executors.newFixedThreadPool(SEEDS_TO_PROCESS);
 		
-//		iterateSeeds(args, executor);
+		iterateSeeds(args, executor);
 		
 //		UNCOMMENT THIS FOR TEST ONLY AND COMMENT THE ONE ABOVE
-		__________________________________________testIterateSeeds(args, executor);
+//		__________________________________________testIterateSeeds(args, executor);
 		
         executor.shutdown();
         
