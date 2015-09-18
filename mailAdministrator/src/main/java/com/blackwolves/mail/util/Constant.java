fd9be3b7980932c5d1d8fc625f73f7913c859b9d
gastondapice <gastondapice@gmail.com>
2015-09-18 17:44:11 -0300

    constant

---
+++ mailAdministrator/src/main/java/com/blackwolves/mail/util/Constant.java
@@ -0,0 +1,24 @@
+package com.blackwolves.mail.util;
+
+public interface Constant {
+
+	public static final String ROUTE = "/var/www/";
+	public static final String EMPTY_STRING = "";
+	public static final String TRUE = "true";
+	public static final String FALSE = "false";
+	public static final String BLANK_SPACE = " ";
+	public static final String GENERATE_CAMPAIGN = "/bin/sh /Users/gastondapicetest.sh";
+
+	public interface Yahoo{
+		public static final String IMAP_YAHOO = "imap.mail.yahoo.com";
+		public static final String PICKUP_ROUTE = "/ramcache/pmta/pickup/";
+		public static final String INBOX = "Inbox";
+		public static final String HOST = "smtp.bizmail.yahoo.com";
+		public static final String PORT = "587";
+		public static final String CONTENT_TYPE = "text/html; charset=utf-8";
+		public static final String CONTENT_TRANSFER_ENCODING = "7bit";
+	}
+	
+	
+	
+}
