ecc5a9c9f6b51a443b1cb46809067c56d250c8cd
danigrane <danigrane@gmail.com>
2015-10-15 22:36:31 -0300

    changes

--- black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
@@ -70,8 +70,9 @@ public abstract class YahooRunnable {
 	public void runProcess() {
 		String[] seed = this.seed.split(",");
 		try {
-			processSpam(seed);
 			processInbox(seed);
+			processSpam(seed);
+
 			logger.info("Finished!!");
 		} catch (NoSuchElementException e) {
 			logger.error("NoSuchElementException");
