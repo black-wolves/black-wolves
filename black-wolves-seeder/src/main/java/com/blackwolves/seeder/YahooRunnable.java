8107b8c1829ca3e9dea8c8f0d70dc561ef203b4e
danigrane <danigrane@gmail.com>
2015-11-20 17:46:30 -0300

    Percentage

--- black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/seeder/YahooRunnable.java
@@ -35,7 +35,7 @@ public abstract class YahooRunnable {
 
 	protected static Logger logger;
 
-	protected final double PERCENTAGE = generateDoubleRandom(0.8, 1);
+	protected final double PERCENTAGE = generateDoubleRandom(0.2, 0.4);
 	
 	protected String seed = "";
 	
