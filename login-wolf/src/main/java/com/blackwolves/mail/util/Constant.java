ba1d6520695c8adc6c6a35af667e3ed03571eb88
gastondapice <gastondapice@gmail.com>
2016-01-21 13:33:01 -0300

    mix multithread added

--- login-wolf/src/main/java/com/blackwolves/mail/util/Constant.java
+++ login-wolf/src/main/java/com/blackwolves/mail/util/Constant.java
@@ -8,6 +8,8 @@ public interface Constant {
 	public static final String TRUE = "true";
 	public static final String FALSE = "false";
 	public static final String BLANK_SPACE = " ";
+	public static final int MAX_SPAM = 20;
+	public static final int DEFAULT_THREADS = 20;
 
 	public interface Yahoo{
 		public static final String IMAP_YAHOO = "imap.mail.yahoo.com";
--- login-wolf/src/main/java/com/blackwolves/mail/yahoo/LoginWolfYahoo.java
+++ login-wolf/src/main/java/com/blackwolves/mail/yahoo/LoginWolfYahoo.java
@@ -1,16 +1,9 @@
 package com.blackwolves.mail.yahoo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.Callable;
 
 import javax.mail.AuthenticationFailedException;
 import javax.mail.Folder;
@@ -22,51 +15,44 @@ import javax.mail.Store;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.com.bytecode.opencsv.CSVReader;
-
 import com.blackwolves.mail.util.Constant;
-import com.blackwolves.mail.util.SeedComparator;
 
 /**
  * @author gastondapice
  *
  */
-public class LoginWolfYahoo {
+public class LoginWolfYahoo implements Callable<List<String[]>>{
 
 	private static Logger logger = LoggerFactory.getLogger(LoginWolfYahoo.class);
-
-	public static void main(String[] args) {
-		
-		String inputFileName = args[0];
-		String outputFileName = args[1];
-		checkAvailableSeeds(inputFileName, outputFileName);
+	
+	List<String[]> list;
+	
+	public LoginWolfYahoo(List<String[]> list) {
+		this.list = list;
 	}
+	
+	@Override
+	public List<String[]> call() throws Exception {
+		return checkAvailableSeeds(list, null);
+	}
+
 
 	/**
 	 * 
 	 * @param inputFileName
 	 * @param outputFileName 
 	 */
-	private static void checkAvailableSeeds(String inputFileName, String outputFileName) {
-		logger.info("Generating contacts list");
-		List<String[]> contacts = generateSeedsList(inputFileName);
-		logger.info("Contact lists generated");
+	private static List<String[]> checkAvailableSeeds(List<String[]> list, String outputFileName) {
 		
 		Properties props = System.getProperties();
 		props.setProperty("mail.store.protocol", "imaps");
 		Session session = Session.getDefaultInstance(props, null);
 		
-		clearFileContent(outputFileName);
-		clearFileContent("specific.csv");
+		List<String[]> result = new ArrayList<String[]>();
 		
-		List<String[]> activeSeeds = new ArrayList<String[]>();
-		List<String[]> activeSeedsWithSpam = new ArrayList<String[]>();
-		List<String[]> activeSeedsWithHugeSpam = new ArrayList<String[]>();
-		List<String[]> inactiveSeeds = new ArrayList<String[]>();
-		
-		logger.info("Seeds to calculate: " + contacts.size());
-		int seedsToGo = contacts.size();
-		for (String[] seed : contacts) {
+		logger.info("Seeds to calculate: " + list.size());
+		int seedsToGo = list.size();
+		for (String[] seed : list) {
 			logger.info("Seeds to go: " + seedsToGo);
 			try{
 				Store store = session.getStore("imaps");
@@ -74,7 +60,6 @@ public class LoginWolfYahoo {
 				logger.info("Starting the connection");
 				store.connect(Constant.Yahoo.IMAP_YAHOO, Constant.Yahoo.IMAP_PORT, seed[0], seed[1]);
 				logger.info("Connected to " + seed[0] + " with pass: " + seed[1]);
-				
 				
 				Folder inbox = store.getFolder(Constant.Yahoo.INBOX);
 				inbox.open(Folder.READ_WRITE);
@@ -88,145 +73,36 @@ public class LoginWolfYahoo {
 				spam.close(true);
 				logger.info("Spam folder closed");
 				
-				String[] newSeed = {seed[0], seed[1], String.valueOf(inboxCount), String.valueOf(spamCount)};
+				String[] newSeed = {seed[0], seed[1], String.valueOf(inboxCount), String.valueOf(spamCount), "true"};
+				result.add(newSeed);
 				store.close();
 				logger.info("Store closed");
 				
-				writeSeedToFile(seed, outputFileName);
-				
-				if(spamCount<=50){
-					activeSeeds.add(newSeed);
-				}else if (spamCount >50 && spamCount <=200){
-					activeSeedsWithSpam.add(newSeed);
-					writeSeedToFile(seed, "specific.csv");
-				}else{
-					activeSeedsWithHugeSpam.add(newSeed);
-					writeSeedToFile(seed, "specific.csv");
-				}
 				--seedsToGo;
 			} catch (NoSuchProviderException e) {
-				writeSeedToFile(seed, outputFileName);
+				String[] newSeed = {seed[0], seed[1], String.valueOf("-1"), String.valueOf("-1"), "true"};
+				result.add(newSeed);
 				logger.error("Error processing seed: " + seed[0] + " with pass: " + seed[1]);
-				logger.error(e.getMessage(), e);
+				logger.error(e.getMessage());
 				--seedsToGo;
 				continue;
 			} catch (AuthenticationFailedException e) {
-				inactiveSeeds.add(seed);
+				String[] newSeed = {seed[0], seed[1], String.valueOf("-1"), String.valueOf("-1"), "false"};
+				result.add(newSeed);
 				logger.error("Error processing seed: " + seed[0] + " with pass: " + seed[1]);
 				logger.error(e.getMessage());
 				--seedsToGo;
 				continue;
 			} catch (MessagingException e) {
-				writeSeedToFile(seed, outputFileName);
+				String[] newSeed = {seed[0], seed[1], String.valueOf("-1"), String.valueOf("-1"), "true"};
+				result.add(newSeed);
 				logger.error("Error processing seed: " + seed[0] + " with pass: " + seed[1]);
-				logger.error(e.getMessage(), e);
+				logger.error(e.getMessage());
 				--seedsToGo;
 				continue;
 			}
 		}
-		Collections.sort(activeSeeds, new SeedComparator<String[]>());
-		Collections.sort(activeSeedsWithSpam, new SeedComparator<String[]>());
-		Collections.sort(activeSeedsWithHugeSpam, new SeedComparator<String[]>());
-		
-		logger.info("");
-		logger.info("Active seeds:");
-		for (String[] seed : activeSeeds) {
-			logger.info(seed[0] + "," + seed[1] + " ACTIVE seed with " + seed[2] + " messages in inbox and " + seed[3] + " messages in spam");
-		}
-		for (String[] seed : activeSeedsWithSpam) {
-			logger.info(seed[0] + "," + seed[1] + " ACTIVE seed with " + seed[2] + " messages in inbox and " + seed[3] + " messages in spam");
-		}
-		for (String[] seed : activeSeedsWithHugeSpam) {
-			logger.info(seed[0] + "," + seed[1] + " ACTIVE seed with " + seed[2] + " messages in inbox and " + seed[3] + " messages in spam");
-		}
-		logger.info("");
-		logger.info("Inactive seeds:");
-		for (String[] seed : inactiveSeeds) {
-			logger.info("Seed " + seed[0] + " with pass: " + seed[1] + " is INACTIVE");
-		}
-		logger.info("");
-		logger.info("Finished validating seeds");
-	}
-
-	/**
-	 * @return
-	 */
-	private static List<String[]> generateSeedsList(String fileName) {
-		List<String[]> seeds = new ArrayList<String[]>();
-		try {
-			CSVReader seedsReader = new CSVReader(new FileReader(Constant.ROUTE + fileName));
-			seeds = seedsReader.readAll();
-		} catch (FileNotFoundException e) {
-			logger.error(e.getMessage(), e);
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		}
-		return seeds;
-	}
-	
-	/**
-	 * 
-	 * @param seed
-	 */
-	private static void writeSeedToFile(String[] seed, String outputFileName) {
-		PrintWriter pw = null;
-		try {
-			List<String> usedSeeds = readSeedsFromFile(outputFileName);
-			pw = new PrintWriter(new FileWriter(Constant.ROUTE + outputFileName));
-			for (String usedSeed : usedSeeds) {
-				pw.write(usedSeed);
-				pw.write("\n");
-			}
-			pw.write(seed[0] + "," + seed[1]);
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		} finally{
-			if(pw!=null){
-				pw.close();
-			}
-		}
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	private static List<String> readSeedsFromFile(String outputFileName) {
-		List<String> list = null;
-		try {
-			File file = new File(Constant.ROUTE + outputFileName);
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			list = new ArrayList<String>();
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				list.add(line);
-			}
-			fileReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return list;
-	}
-	
-	/**
-	 * 
-	 * @param outputFileName
-	 */
-	private static void clearFileContent(String outputFileName){
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter(Constant.ROUTE + outputFileName);
-			writer.print("");
-			writer.close();
-		} catch (FileNotFoundException e) {
-			logger.error(e.getMessage(), e);
-		} finally{
-			if(writer!=null){
-				writer.close();
-			}
-		}
-		
+		return result;
 	}
 
 }
