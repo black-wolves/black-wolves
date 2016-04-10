3c43fe7e726db2a9721e5ab6b47340779af9d8dc
danigrane <danigrane@gmail.com>
2016-04-09 23:49:33 -0300

    Less accounts more frecuently

--- black-wolves-seeder/src/main/java/com/blackwolves/seeder/util/JDBC.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/seeder/util/JDBC.java
@@ -69,7 +69,7 @@ public class JDBC {
 			
 			String selectSQL = "SELECT * FROM "
 					+ "(SELECT * FROM FEEDER WHERE FEEDER_UPDATED_DATE IS NOT NULL order by FEEDER_UPDATED_DATE DESC) x "
-					+ "ORDER BY x.SEEDER_UPDATED_DATE ASC LIMIT 100";
+					+ "ORDER BY x.SEEDER_UPDATED_DATE ASC LIMIT 25";
 			
 //			String selectSQL =  "SELECT * FROM FEEDER  where SEED='voxackkhzzc@yahoo.com'";
 			
