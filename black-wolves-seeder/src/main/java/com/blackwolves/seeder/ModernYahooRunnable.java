6aafce0e8d8941dffde5b9fd82659c817b4bd968
danigrane <danigrane@gmail.com>
2016-04-15 16:40:39 -0300

    LGE

--- black-wolves-seeder/src/main/java/com/blackwolves/seeder/ModernYahooRunnable.java
+++ black-wolves-seeder/src/main/java/com/blackwolves/seeder/ModernYahooRunnable.java
@@ -82,9 +82,9 @@ public class ModernYahooRunnable extends YahooRunnable {
 						currentMsg = null;
 						// Looking for MyMessage
 						if (findMyMessage() && !foundMyMsg) {
-							currentMsg = findMessage(inboxMsgs, Constant.FROM.YAHOO);
+							currentMsg = findMessage(inboxMsgs, Constant.FROM.LGE);
 							if (currentMsg != null) {
-								logger.info(" #########  MAIL "+Constant.FROM.YAHOO +" FOUND AT INBOX ##############");
+								logger.info(" #########  MAIL "+Constant.FROM.LGE +" FOUND AT INBOX ##############");
 								foundMyMsg = true;
 							}
 
@@ -110,7 +110,7 @@ public class ModernYahooRunnable extends YahooRunnable {
 							if (isClickable(driver, currentMsg)) {
 								logger.info("Will click at  X: " + currentMsg.getLocation().getX() + " and Y:" + currentMsg.getLocation().getY());
 								currentMsg.click();
-								if (fromText.contains(Constant.FROM.YAHOO)) {
+								if (fromText.contains(Constant.FROM.LGE)) {
 									opened = true;
 									if (Math.random() <= 0.6) {
 										clickShowImages("show-text");
