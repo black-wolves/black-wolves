533a76f36a6ca558628b48967a61f9c7677c4098
gastondapice <gastondapice@gmail.com>
2016-04-28 08:35:37 -0300

    updates

--- subscriber/src/main/java/com/blackwolves/subscriber/util/JDBC.java
+++ subscriber/src/main/java/com/blackwolves/subscriber/util/JDBC.java
@@ -46,7 +46,7 @@ public class JDBC {
 		TimeZone tz = TimeZone.getTimeZone(Constant.JDBC.GMT_3);
 		formatter.setTimeZone(tz);
 		
-		String selectSQL = "SELECT * FROM FEEDER ORDER BY RAND() WHERE SUBSCRIPTION IS NULL LIMIT 50";
+		String selectSQL = "SELECT * FROM FEEDER WHERE SUBSCRIPTION is null ORDER BY RAND() LIMIT 50";
 		List<Seed> seeds = new ArrayList<Seed>();
 		try {
 			dbConnection = getDBConnection();
